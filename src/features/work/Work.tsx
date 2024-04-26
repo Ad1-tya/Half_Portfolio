33652a1b972d8fc875ed765e546213f2ef0e1575
Ad1-tya <076bei003.aditya@pcampus.edu.np>
2024-04-26 10:55:39 +0545

    Update Work.tsx

--- src/features/work/Work.tsx
+++ src/features/work/Work.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { WorkData } from '@/data';
 
 export const Work = () => {
