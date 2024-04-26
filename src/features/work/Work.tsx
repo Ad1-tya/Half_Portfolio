c42504be5c097a35bdb4fdd726d0c059a3af3e7e
Ad1-tya <076bei003.aditya@pcampus.edu.np>
2024-04-26 22:59:32 +0545

    Changed the import alphabetically

--- src/features/work/Work.tsx
+++ src/features/work/Work.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { WorkData } from '@/data';
 
 export const Work = () => {
